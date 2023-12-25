6fa95ff42f45aa7690c8a5632c26bf39d244b44d
wajihMahrouseh <wajiho.mahrouseh@gmail.com>
2023-12-25 10:35:23 +0300

    prepare messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | 
+    | 
+    | 
+    |
+    */
+
+    'add' => 'added successfully',
+    'edit' => 'edited successfully',
+    'delete' => 'deleted successfully',
+
+    'incorrectCredentials' => 'Invalid username or password. Please try again.',
+    'loginWithDifferentDevice' => 'You are attempting to login from a different device. Please login from the previously used device.',
+    'loggedOutSuccessfully' => 'You have been logged out successfully.',
+];
